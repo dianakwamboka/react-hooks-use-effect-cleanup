140f33b63af57b957724d2007ef744ea5d4a1dbb
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-10-06 10:16:47 +0300

    Completed Assignment

--- src/Clock.js
+++ src/Clock.js
@@ -4,12 +4,15 @@ function Clock() {
   const [time, setTime] = useState(new Date());
 
   useEffect(() => {
-    setInterval(() => {
+    const intervalId = setInterval(() => {
       setTime(new Date());
     }, 1000);
+
+    return function cleanup(){
+      clearInterval(intervalId)
+    }
   }, []);
 
   return <div>{time.toString()}</div>;
 }
-
 export default Clock;
